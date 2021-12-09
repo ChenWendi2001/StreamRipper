23a941cf5c956d53f4315c93a6c86ba4fdd99f87
CWHer <cwher@outlook.com>
2021-12-09 23:05:57 +0800

    feat: add P2P downloader

---
+++ src/Middleware/P2P/server.py
@@ -0,0 +1,36 @@
+import asyncio
+import threading
+
+
+class Server(threading.Thread):
+    def __init__(self, local_db, host_ip, port=4003) -> None:
+        super().__init__(daemon=True)
+        self.local_db = local_db
+        self.host_ip = host_ip
+        self.port = port
+
+    async def query(self, reader, writer):
+        data = await reader.read(100)
+        key = data.decode()
+
+        addr = writer.get_extra_info("peername")
+        print(f"Received {key} from {addr}")
+
+        data = self.local_db.query(key)
+        writer.write(data.encode())
+        await writer.drain()
+
+        print("Close the connection")
+        writer.close()
+
+    async def listen(self):
+        server = await asyncio.start_server(
+            self.query, self.host_ip, self.port)
+
+        print(f"Serving on {self.host_ip}:{self.port}")
+
+        async with server:
+            await server.serve_forever()
+
+    def run(self):
+        asyncio.run(self.listen())
