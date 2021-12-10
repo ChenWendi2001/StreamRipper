aace722c66d4bf7e2f035779f9d86518b0d34af2
Wendi Chen <1277998401@qq.com>
2021-12-10 09:19:01 +0800

    feat: support basic demo for frontend

--- src/frontend.py
+++ src/frontend.py
@@ -3,13 +3,15 @@ from mitmproxy import ctx, http
 
 from enum import Enum
 
+from mitmproxy.net.http import response
+
 class Status(Enum):
     OK = 1
     Error = 2
 
+database = {}
 
 class Router:
-
     def request(self, flow: mitmproxy.http.HTTPFlow):
         # only intercept GET requests
         request = flow.request
@@ -18,11 +20,22 @@ class Router:
             if status == Status.OK:
                 ctx.log.info("OK")
                 ctx.log.info(key)
+                if key in database:
+                    ctx.log.info("********** hit **********")
+                    response = database[key]
+                    response.headers["server"] = "chenwendiiiii"
+                    flow.response = database[key]
             else:
                 ctx.log.info("Error")
 
     def response(self, flow: mitmproxy.http.HTTPFlow):
         #TODO
+        request = flow.request
+        if request.method == "GET":
+            status,key = self.get_key_from_request(request)
+            if status == Status.OK:
+                if key not in database:
+                    database[key] = flow.response
         pass
 
     def http_connect(self, flow: mitmproxy.http.HTTPFlow):  
@@ -32,7 +45,7 @@ class Router:
     def get_key_from_request(self, request):
         # get file name
         path = request.path
-        filename = path[:path.find(".m4s")].split("/")[-1].split("-")[-1]
+        filename = path[:path.find(".m4s")].split("/")[-1]
 
         # request header (contains referer and range)
         header = request.headers
