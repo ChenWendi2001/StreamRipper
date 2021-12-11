f008efb0c3d59cca5c8ac668945dd6fbeef07726
CWHer <cwher@outlook.com>
2021-12-11 13:23:41 +0800

    feat: add scheduler

---
+++ src/Middleware/scheduler/speed_test.py
@@ -0,0 +1,27 @@
+import threading
+import time
+
+import speedtest
+from utils import printInfo
+
+
+class SpeedTester(threading.Thread):
+    def __init__(self, backend, period=600) -> None:
+        super().__init__(daemon=True)
+        self.backend = backend
+        self.period = period
+        self.speed_test = speedtest.Speedtest()
+
+    def run(self):
+        while True:
+            self.speed_test.get_best_server()
+            self.speed_test.download(threads=0)
+            results = self.speed_test.results.dict()
+            download_speed = results["download"] / 1048576
+            printInfo("download speed {:>.4f}".format(download_speed))
+
+            if not self.backend is None:
+                self.backend.modify(
+                    "STATUS", (self.backend.host_ip, download_speed))
+
+            time.sleep(self.period)
